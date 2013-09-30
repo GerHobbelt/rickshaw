b281ca7cc194c2e3fc29182c696229377dc6609e
Tom <bballts14@gmx.de>
2013-09-30 14:23:32 +1000

    Limit number of lines shown in legend.

---
+++ src/js/Rickshaw.Graph.Behavior.Series.DetailLegend.js
@@ -0,0 +1,46 @@
+Rickshaw.namespace('Rickshaw.Graph.Behavior.Series.DetailLegend');
+
+Rickshaw.Graph.Behavior.Series.DetailLegend = function(args) {
+  var self = this;
+
+  this.graph         = args.graph;
+  this.legend        = args.legend;
+  this.elements      = args.elements;
+  this.defaultDetail = args.defaultDetail;
+
+  this.highlightDetail = function(detail) {
+    for (var j = 0; j < self.elements.length; ++j) {
+      var element = self.elements[j];
+
+      if (detail === element.getAttribute('data-detail')) {
+        element.classList.remove('disabled');
+      } else {
+        element.classList.add('disabled');
+      }
+    }
+  };
+
+  this.enableLinesForDetail = function(detail) {
+    self.legend.lines.forEach(function(line) {
+      if (line.series.detail === detail) {
+        line.series.enable();
+        line.element.classList.remove('hidden');
+      } else {
+        line.series.disable();
+        line.element.classList.add('hidden');
+      }
+    });
+  };
+
+  for (var i = 0; i < self.elements.length; ++i) {
+    self.elements[i].addEventListener('click', function(e) {
+      var detail = e.target.parentNode.getAttribute('data-detail');
+
+      self.highlightDetail(detail);
+      self.enableLinesForDetail(detail);
+    });
+  }
+
+  self.highlightDetail(self.defaultDetail);
+  self.enableLinesForDetail(self.defaultDetail);
+};
